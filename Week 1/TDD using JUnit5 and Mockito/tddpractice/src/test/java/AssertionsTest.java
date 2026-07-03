54a509131f660a97171faf865c2365e919590b4a
Rushil-Walia <rushiltiger@gmail.com>
2026-07-03 05:53:27 +0530

    Spring Core and Maven

--- Week 1/TDD using JUnit5 and Mockito/tddpractice/src/test/java/AssertionsTest.java
+++ Week 1/TDD using JUnit5 and Mockito/tddpractice/src/test/java/AssertionsTest.java
@@ -24,7 +24,7 @@ public class AssertionsTest {
 
         // 6. Assert Throws (JUnit 5 Specific): Verifies that a specific exception is thrown
         assertThrows(ArithmeticException.class, () -> {
-            int result = 10 / 0;
+            int dummy  = 10 / 0;
         }, "Dividing by zero must throw an ArithmeticException");
     }
 }
